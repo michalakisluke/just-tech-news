203a44309b675689244b16fcea50b310bfe6c039
Luke Michalakis <michalakisluke@gmail.com>
2021-11-01 18:23:00 -0400

    <Build login / logout features>

--- server.js
+++ server.js
@@ -2,6 +2,9 @@ const path = require('path');
 const express = require('express');
 const routes = require('./controllers/');
 const sequelize = require('./config/connection');
+const session = require('express-session');
+
+const SequelizeStore = require('connect-session-sequelize')(session.Store)
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -9,12 +12,23 @@ const PORT = process.env.PORT || 3001;
 const exphbs = require('express-handlebars');
 const hbs = exphbs.create({});
 
+const sess = {
+    secret: 'Super secret secret',
+    cookie: {},
+    resave: false,
+    saveUnitialized: true,
+    store: new SequelizeStore({
+        db: sequelize
+    })
+};
+
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(session(sess));
 
 //turn on routes
 app.use(routes);
