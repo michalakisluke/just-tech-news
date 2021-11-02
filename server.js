90e30a652c61a61ad808b16009a255e750ea0ba4
Luke Michalakis <michalakisluke@gmail.com>
2021-11-02 16:40:54 -0400

    <Use handlebar helper functions>

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const express = require('express');
 const routes = require('./controllers/');
 const sequelize = require('./config/connection');
 const session = require('express-session');
+const helpers = require('./utils/helpers');
 
 const SequelizeStore = require('connect-session-sequelize')(session.Store)
 
@@ -10,7 +11,7 @@ const app = express();
 const PORT = process.env.PORT || 3001;
 
 const exphbs = require('express-handlebars');
-const hbs = exphbs.create({});
+const hbs = exphbs.create({ helpers });
 
 const sess = {
     secret: 'Super secret secret',
