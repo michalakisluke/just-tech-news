0636e3b0539060bd127b205ab7d03725ebfa9615
Luke Michalakis <michalakisluke@gmail.com>
2021-11-03 13:58:01 -0400

    <Build dashboard>

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -1,5 +1,6 @@
 const router = require('express').Router();
 const { User, Post, Vote } = require("../../models");
+const withAuth = require('../../utils/auth');
 
 // GET/api/users
 router.get('/', (req, res) => {
@@ -56,7 +57,7 @@ router.get('/:id', (req, res) => {
 });
 
 // POST/api/users
-router.post('/', (req, res) => {
+router.post('/', withAuth, (req, res) => {
     User.create({
         username: req.body.username,
         email: req.body.email,
@@ -74,7 +75,7 @@ router.post('/', (req, res) => {
 });
 
 // PUT/api/users/1
-router.put('/:id', (req, res) => {
+router.put('/:id', withAuth, (req, res) => {
     User.update(req.body, {
         individualHooks: true,
         where: {
@@ -95,7 +96,7 @@ router.put('/:id', (req, res) => {
 });
 
 // DELETE/api/users/1
-router.delete('/:id', (req, res) => {
+router.delete('/:id', withAuth, (req, res) => {
     User.destroy({
         where: {
             id: req.params.id
@@ -145,7 +146,7 @@ router.post('/login', (req, res) => {
     });
 });
 
-router.post('/logout', (req, res) => {
+router.post('/logout',  withAuth, (req, res) => {
     if (req.session.loggedIn) {
         req.session.destroy(() => {
             res.status(204).end();
